315604cdbb6ffbc8b640f203ad3e9cc3f848f6cb
Theo Gainey <theo@heysparkbox.com>
2022-03-30 14:35:06 -0400

    test: test for error handling
    add test for proper error handling
    
    Changes to be committed:
    	modified:   src/data.test.ts

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,7 +1,8 @@
 import { LocData } from './types.d.ts';
-import { assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
+import sinon from "https://cdn.skypack.dev/sinon";
+import { assertEquals, assertRejects } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
 import { fetchLocation } from './data.ts';
-Deno.test('fetchLocation', async () => {
+Deno.test('fetchLocation returns only the needed data after successful request', async () => {
   const res = new TextEncoder().encode(JSON.stringify({
     ip: 'string',
     hostname: 'string',
@@ -26,4 +27,33 @@ Deno.test('fetchLocation', async () => {
   assertEquals(result, expected);
 
   globalThis.fetch = globalFetchRef;
+});
+
+Deno.test('fetchLocation handles errors with console.log', async () => {
+
+  const globalFetchRef = globalThis.fetch;
+  const globalConsoleLogRef = globalThis.console.log;
+
+  const res = new TextEncoder().encode(JSON.stringify({
+    ip: 'string',
+    hostname: 'string',
+    city: 'Dayton',
+    region: 'Ohio',
+    country: 'US',
+    loc: '39.6464,-84.1717',
+    org: 'string',
+    postal: '45459',
+    timezone: 'America/New_York',
+  }));
+
+  globalThis.fetch = () => Promise.reject(new Response(res));
+  globalThis.console.log =  ():string => 'console.log handled error';
+  
+
+  const result = await fetchLocation();
+  assertEquals(result, 'console.log handled error');
+
+  globalThis.fetch = globalFetchRef;
+  globalThis.console.log = globalConsoleLogRef  
+
 });
