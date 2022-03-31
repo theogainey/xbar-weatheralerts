e13bf2e4829437306fb86ffc4fa0a35bf3cd7702
Theo Gainey <theo@heysparkbox.com>
2022-03-31 10:31:23 -0400

    test: add additonal tests
    Changes to be committed:
    	modified:   src/data.test.ts

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,6 +1,74 @@
 import { LocData } from './types.d.ts';
 import { assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
-import { fetchLocation } from './data.ts';
+import { fetchLocation, fetchWrapper } from './data.ts';
+
+Deno.test('fetchWrapper returns expected data with default handler', async () => {
+  const globalFetchRef = globalThis.fetch;
+
+  const res = new TextEncoder().encode(JSON.stringify({
+    test: 'pass',
+  }));
+
+  globalThis.fetch = () => Promise.resolve(new Response(res));
+
+  const result = await fetchWrapper({ url: 'test.test' })();
+  const expected = { test: 'pass' };
+  assertEquals(result, expected);
+  globalThis.fetch = globalFetchRef;
+});
+
+Deno.test('fetchWrapper returns expected data with custom handler', async () => {
+  const globalFetchRef = globalThis.fetch;
+
+  const res = new TextEncoder().encode(JSON.stringify({
+    test: 'pass',
+  }));
+
+  globalThis.fetch = () => Promise.resolve(new Response(res));
+
+  const result = await fetchWrapper({
+    url: 'test.test',
+    dataHandler: (data: { test: string }) => data.test,
+  })();
+  const expected = 'pass';
+  assertEquals(result, expected);
+  globalThis.fetch = globalFetchRef;
+});
+
+Deno.test('fetchWrapper default error handler handles errors with console.log', async () => {
+  const globalFetchRef = globalThis.fetch;
+  const globalConsoleLogRef = globalThis.console.log;
+
+  const res = new TextEncoder().encode(JSON.stringify({
+    test: 'failing',
+  }));
+
+  globalThis.fetch = () => Promise.reject(new Response(res));
+  globalThis.console.log = (): string => 'console.log handled error';
+
+  const result = await fetchWrapper({ url: 'test.test' })();
+  assertEquals(result, 'console.log handled error');
+
+  globalThis.fetch = globalFetchRef;
+  globalThis.console.log = globalConsoleLogRef;
+});
+
+Deno.test('fetchWrapper returns expected error response with custom handler', async () => {
+  const globalFetchRef = globalThis.fetch;
+
+  const res = new TextEncoder().encode(JSON.stringify({
+    test: 'failing',
+  }));
+
+  globalThis.fetch = () => Promise.reject(new Response(res));
+  const result = await fetchWrapper({
+    url: 'test.test',
+    errorHandler: () => 'custom handler handled error',
+  })();
+  assertEquals(result, 'custom handler handled error');
+  globalThis.fetch = globalFetchRef;
+});
+
 Deno.test('fetchLocation returns only the needed data after successful request', async () => {
   const res = new TextEncoder().encode(JSON.stringify({
     ip: 'string',
