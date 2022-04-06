f2a1df21d1514ccbb3ad1901448d1564d2432071
Theo Gainey <theo@heysparkbox.com>
2022-04-06 10:00:50 -0400

    rework: remove defensive coding
    remove defensive coding patterns
    
    Changes to be committed:
    	modified:   src/data.test.ts
    	modified:   src/data.ts

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,9 +1,11 @@
 import { LocData } from './types.d.ts';
-import { assertEquals } from 'https://deno.land/std@0.128.0/testing/asserts.ts';
+import {
+  assert,
+  assertEquals,
+} from 'https://deno.land/std@0.128.0/testing/asserts.ts';
 import { fetchLocation, fetchNWS } from './data.ts';
 
 const globalFetchRef = globalThis.fetch;
-const globalConsoleLogRef = globalThis.console.log;
 
 Deno.test('fetchLocation returns only the needed data after successful request', async () => {
   const res = new TextEncoder().encode(JSON.stringify({
@@ -24,21 +26,23 @@ Deno.test('fetchLocation returns only the needed data after successful request',
     region: 'Ohio',
     coordinates: '39.6464,-84.1717',
   };
-  const result: LocData | void = await fetchLocation();
+  const result: LocData = await fetchLocation();
   assertEquals(result, expected);
 
   globalThis.fetch = globalFetchRef;
 });
 
-Deno.test('fetchLocation handles errors with console.log', async () => {
+Deno.test('fetchLocation does not handle rejected Promise', async () => {
   globalThis.fetch = () => Promise.reject(new Response());
-  globalThis.console.log = (): string => 'console.log handled error';
 
-  const result = await fetchLocation();
-  assertEquals(result, 'console.log handled error');
+  try {
+    await fetchLocation();
+    assert(false);
+  } catch (_err) {
+    assert(true);
+  }
 
   globalThis.fetch = globalFetchRef;
-  globalThis.console.log = globalConsoleLogRef;
 });
 
 Deno.test('fetchNWS calls NWS api with correctly formated url', async () => {
@@ -65,19 +69,20 @@ Deno.test('fetchNWS calls NWS api with correctly formated url', async () => {
   globalThis.fetch = globalFetchRef;
 });
 
-Deno.test('fetchNWS handles errors with console.log ', async () => {
-  globalThis.fetch = () => Promise.reject(new Response());
-  globalThis.console.log = (): string => 'console.log handled error';
+Deno.test('fetchNWS does not handle rejected Promise ', async () => {
+  globalThis.fetch = () => Promise.resolve(new Response());
 
   const testData: LocData = {
     city: 'Dayton',
     region: 'Ohio',
     coordinates: '39.6464,-84.1717',
   };
-
-  const result = await fetchNWS(testData);
-  assertEquals(result, 'console.log handled error');
+  try {
+    await fetchNWS(testData);
+    assert(false);
+  } catch (_err) {
+    assert(true);
+  }
 
   globalThis.fetch = globalFetchRef;
-  globalThis.console.log = globalConsoleLogRef;
 });
--- src/data.ts
+++ src/data.ts
@@ -12,29 +12,19 @@ const trimLocation = (
 };
 
 /**  wrapped instance of fetch for calling location api*/
-export const fetchLocation = async (): Promise<void | LocData> => {
-  try {
-    const res = await fetch('http://ipinfo.io/json');
-    const data = await res.json();
-    return trimLocation(data);
-  } catch (err) {
-    return console.log(err);
-  }
+export const fetchLocation = async (): Promise<LocData> => {
+  const res = await fetch('http://ipinfo.io/json');
+  const data = await res.json();
+  return trimLocation(data);
 };
 
 /**  wrapped instance of fetch for calling NWS api*/
 export const fetchNWS = async (
-  data: LocData | void,
+  data: LocData,
 ) => {
-  if (data) {
-    try {
-      const res = await fetch(
-        `https://api.weather.gov/alerts/active?point=${data.coordinates}`,
-      );
-      const nwsData = await res.json();
-      return nwsData;
-    } catch (err) {
-      return console.log(err);
-    }
-  }
+  const res = await fetch(
+    `https://api.weather.gov/alerts/active?point=${data.coordinates}`,
+  );
+  const nwsData = await res.json();
+  return nwsData;
 };
