e86002ac3d1ca3b3c82fd341a5b494314bbbad4a
Theo Gainey <theo@heysparkbox.com>
2022-05-09 13:57:06 -0400

    feat: update imports
    update weatherAlerts.10min.ts imports

--- weatherAlerts.10min.ts
+++ weatherAlerts.10min.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env /path/to/the/deno/executable run --allow-net 
 import { separator, xbar } from 'https://deno.land/x/xbar@v0.3.0/mod.ts';
-import { fetchLocation, fetchWeatherData } from './src/data.ts';
-import { formatForXbar } from './src/format.ts';
+import {
+  fetchLocation,
+  fetchWeatherData,
+  formatForXbar,
+} from 'https://raw.githubusercontent.com/theogainey/xbar-weatheralerts/main/src/index.ts';
 
 fetchLocation()
   .then((loc) => fetchWeatherData(loc))
